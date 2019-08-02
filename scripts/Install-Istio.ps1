1d66ac5c92650a71946e4ffdb00c17f1b1c1294b
Anders Sørensen <mail@anars.dk>
2019-08-02 12:24:51 +0200

    Work2 gi

---
+++ scripts/Install-Istio.ps1
@@ -0,0 +1,4 @@
+$ISTIO_VERSION = '1.2.2'
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -URI "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-win.zip" -OutFile "istio-$ISTIO_VERSION.zip"
+Expand-Archive -Path "istio-$ISTIO_VERSION.zip" -DestinationPath .
